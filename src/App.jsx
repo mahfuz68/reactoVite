cb95d52c82a383822ceac455ac7fc3092bea3662
MAHFUZ ANAM <mymsicret34@gmail.com>
2022-03-19 19:20:30 +0600

    practice rander props & hoc

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import Calculator from './component/Calculator';
+import ClickCounter from './component/renderProps/ClickCounter';
+import Counter from './component/renderProps/Counter';
+import HoverCounter from './component/renderProps/HoverCounter';
 
 function App() {
   return (
     <div>
       {/* <ClockList quantities={[1, 2, 3==]} /> */}
       {/* <Form /> */}
-      <Calculator />
+      {/* <Calculator /> */}
+      {/* <ClickCounter /> */}
+      <Counter render={
+        (count, incrementCount) => (<ClickCounter count={count} incrementCount={incrementCount} />)
+      }
+      />
+      <Counter render={
+        (count, incrementCount) => (<HoverCounter count={count} incrementCount={incrementCount} />)
+      }
+      />
     </div>
   );
 }
---
+++ src/component/renderProps/ClickCounter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class ClickCounter extends React.Component {
+  render() {
+    const { count, incrementCount } = this.props;
+    { console.log(count, 'thisis'); }
+    return (
+      <>
+        <button className="mt-4" type="button" onClick={incrementCount}>
+          Clicked
+          {' '}
+          {count}
+          {' '}
+          time
+        </button>
+        <br />
+
+      </>
+    );
+  }
+}
