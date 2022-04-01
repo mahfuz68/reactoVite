ddf55b6a733fa4bddec00fb0da936e7ec37a842c
MAHFUZ ANAM <mymsicret34@gmail.com>
2022-04-01 09:59:52 +0600

    firebse autehentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,48 @@
 import { Component } from 'react';
-import Form from './component/Hook/UseRef&ForowardRef/Form';
+// import Form from './component/Hook/UseRef&ForowardRef/Form';
+import Apps from './component/fire/Apps';
 
 class App extends Component {
-  state = {
-    theme: 'dark',
-    switchTheme: () => {
-      this.setState(({ theme }) => {
-        if (theme === 'dark') {
-          return { theme: 'light' };
+    state = {
+        theme: 'dark',
+        switchTheme: () => {
+            this.setState(({ theme }) => {
+                if (theme === 'dark') {
+                    return { theme: 'light' };
+                }
+                return { theme: 'dark' };
+            });
         }
-        return { theme: 'dark' };
-      });
-    },
-  };
+    };
 
-  render() {
-    const { theme, switchTheme } = this.state;
-    return (
-      <div>
-        {/* <ClockList quantities={[1, 2, 3==]} /> */}
-        {/* <Form /> */}
-        {/* <Calculator /> */}
-        {/* <ClickCounter /> */}
-        {/* <Counter render={
+    render() {
+        const { theme, switchTheme } = this.state;
+        return (
+            <div>
+                {/* <ClockList quantities={[1, 2, 3==]} /> */}
+                {/* <Form /> */}
+                {/* <Calculator /> */}
+                {/* <ClickCounter /> */}
+                {/* <Counter render={
         (count, incrementCount) => (<ClickCounter count={count} incrementCount={incrementCount} />)
       } */
-        // />
-    }
-        {/* <ThemeContext.Provider value={this.state}>
+                    // />
+                }
+                {/* <ThemeContext.Provider value={this.state}>
           <Content />
         </ThemeContext.Provider> */}
-        {/* <TestForm /> */}
-        {/* <Counter /> */}
-        {/* <MycomponentClass /> */}
-        {/* <MyComponentFunction /> */}
-        {/* <Condition /> */}
-        {/* <Mother /> */}
-        <Form />
-        {/* <Time /> */}
-      </div>
-    );
-  }
+                {/* <TestForm /> */}
+                {/* <Counter /> */}
+                {/* <MycomponentClass /> */}
+                {/* <MyComponentFunction /> */}
+                {/* <Condition /> */}
+                {/* <Mother /> */}
+                {/* <Form /> */}
+                {/* <Time /> */}
+                <Apps />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/component/fire/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: import.meta.env.API_KEY,
+    authDomain: import.meta.env.AUTH_DOMAIN,
+    databaseURL: import.meta.env.DATABASE_URL,
+    projectId: import.meta.env.PROJECT_ID,
+    storageBucket: import.meta.env.STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+    appId: import.meta.env.APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
